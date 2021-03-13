77d07c392454c4298ae7bb96d9e9b24fb14ec79a
substack <substack@bits.coop>
2021-03-12 23:01:27 -1000

    bounds clipping

--- example/main.js
+++ example/main.js
@@ -1,6 +1,40 @@
 var regl = require('regl')()
 var labelEngine = require('../')({
   types: {
+    bounds: {
+      size: function (out) {
+        out.cells = 0
+        out.positions = 0
+        out.bounds = 20
+      },
+      write: function (out, f) {
+        var xmin = f.bounds[0]
+        var ymin = f.bounds[1]
+        var xmax = f.bounds[2]
+        var ymax = f.bounds[3]
+        var dx = 0.01, dy = 0.01
+        out.bounds.data[out.bounds.offset++] = xmin
+        out.bounds.data[out.bounds.offset++] = ymin
+        out.bounds.data[out.bounds.offset++] = xmin
+        out.bounds.data[out.bounds.offset++] = ymax
+        out.bounds.data[out.bounds.offset++] = xmax
+        out.bounds.data[out.bounds.offset++] = ymax
+        out.bounds.data[out.bounds.offset++] = xmax
+        out.bounds.data[out.bounds.offset++] = ymin
+        out.bounds.data[out.bounds.offset++] = xmin
+        out.bounds.data[out.bounds.offset++] = ymin
+        out.bounds.data[out.bounds.offset++] = xmin - dx
+        out.bounds.data[out.bounds.offset++] = ymin - dy
+        out.bounds.data[out.bounds.offset++] = xmax + dx
+        out.bounds.data[out.bounds.offset++] = ymin - dy
+        out.bounds.data[out.bounds.offset++] = xmax + dx
+        out.bounds.data[out.bounds.offset++] = ymax + dy
+        out.bounds.data[out.bounds.offset++] = xmin - dx
+        out.bounds.data[out.bounds.offset++] = ymax + dy
+        out.bounds.data[out.bounds.offset++] = xmin - dx
+        out.bounds.data[out.bounds.offset++] = ymin - dy
+      }
+    },
     point: {
       size: function (out) {
         out.cells = 6
@@ -122,7 +156,9 @@ var labelEngine = require('../')({
   }
 })
 
-var labels = []
+var labels = [
+  { type: 'bounds', bounds: [-1,-1,+1,+1] }
+]
 for (var i = 0; i < 150; i++) {
   labels.push({
     type: 'point',

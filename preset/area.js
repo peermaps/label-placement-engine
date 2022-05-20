3cb9ac9bc7c8594bdd4d66572b356acabd6ff7c6
substack <substack@bits.coop>
2022-05-19 23:34:57 -1000

    rewrite area preset, same interface

--- preset/area.js
+++ preset/area.js
@@ -1,8 +1,8 @@
 var vec2set = require('gl-vec2/set')
 var ptest = require('polygon-intersect-test/flat')
 var defaultScale = [1,1]
-var v0 = [0,0], v1 = [0,0], v2 = [0,0], v3 = [0,0]
-var P = [0,0,0,0,0,0,0,0]
+var X = [0,0,0,0,0,0,0,0]
+var C = [0,0]
 
 module.exports = function (params) {
   if (!params) params = {}
@@ -10,9 +10,19 @@ module.exports = function (params) {
     size: function (out, f) {
       out.cells = 6
       out.positions = 8
-      out.bounds = 8
+      out.bounds = 16
     },
     write: function (out, f) {
+      vec2set(C, 0, 0)
+      var positions = f.positions || params.positions
+      if (positions.length < 2) return
+      var n = positions.length/2
+      for (var i = 0; i < positions.length; i+=2) {
+        var x = positions[i+0], y = positions[i+1]
+        if (!isNaN(x)) C[0] += x/n
+        if (!isNaN(y)) C[1] += y/n
+      }
+
       var scale = f.scale || params.scale || defaultScale
       var positions = f.positions || params.positions
       var positionsScale = f.positionsScale || params.positionsScale || scale
@@ -27,51 +37,21 @@ module.exports = function (params) {
       var labelMargin1 = labelMargin[1]*labelMarginScale[1]
       var aspect = f.aspect || params.aspect || 1
 
-      var cx = 0, cy = 0, n = positions.length/2
-      for (var i = 0; i < positions.length; i+=2) {
-        cx += positions[i+0]/n
-        cy += positions[i+1]/n
-      }
-      var lsx = labelSize0*aspect/2
-      var lsy = labelSize1/2
-      var x0 = cx - lsx
-      var x1 = cx + lsx
-      var y0 = cy - lsy
-      var y1 = cy + lsy
-      vec2set(v0, x0, y0)
-      vec2set(v1, x1, y0)
-      vec2set(v2, x1, y1)
-      vec2set(v3, x0, y1)
-      pset(P, v0, v1, v2, v3)
-      if (!ptest(P, positions)) return // only show if label intersects polygon
+      var xmin = C[0] - labelSize0*0.5
+      var xmax = C[0] + labelSize0*0.5
+      var ymin = C[1] - labelSize1*0.5
+      var ymax = C[1] + labelSize1*0.5
+      pset(X, xmin, xmax, ymin, ymax)
+      if (!ptest(X, positions)) return // only show if label intersects polygon
 
-      out.positions.data[out.positions.offset++] = v0[0]/aspect
-      out.positions.data[out.positions.offset++] = v0[1]
-      out.positions.data[out.positions.offset++] = v1[0]/aspect
-      out.positions.data[out.positions.offset++] = v1[1]
-      out.positions.data[out.positions.offset++] = v2[0]/aspect
-      out.positions.data[out.positions.offset++] = v2[1]
-      out.positions.data[out.positions.offset++] = v3[0]/aspect
-      out.positions.data[out.positions.offset++] = v3[1]
-
-      var px = labelMargin0*aspect
-      var py = labelMargin1
-      x0 -= px
-      x1 += px
-      y0 -= py
-      y1 += py
-      vec2set(v0, x0, y0)
-      vec2set(v1, x1, y0)
-      vec2set(v2, x1, y1)
-      vec2set(v3, x0, y1)
-      out.bounds.data[out.bounds.offset++] = v0[0]/aspect
-      out.bounds.data[out.bounds.offset++] = v0[1]
-      out.bounds.data[out.bounds.offset++] = v1[0]/aspect
-      out.bounds.data[out.bounds.offset++] = v1[1]
-      out.bounds.data[out.bounds.offset++] = v2[0]/aspect
-      out.bounds.data[out.bounds.offset++] = v2[1]
-      out.bounds.data[out.bounds.offset++] = v3[0]/aspect
-      out.bounds.data[out.bounds.offset++] = v3[1]
+      out.bounds.data[out.bounds.offset++] = xmin - labelMargin0
+      out.bounds.data[out.bounds.offset++] = ymin - labelMargin1
+      out.bounds.data[out.bounds.offset++] = xmax + labelMargin0
+      out.bounds.data[out.bounds.offset++] = ymin - labelMargin1
+      out.bounds.data[out.bounds.offset++] = xmax + labelMargin0
+      out.bounds.data[out.bounds.offset++] = ymax + labelMargin1
+      out.bounds.data[out.bounds.offset++] = xmin - labelMargin0
+      out.bounds.data[out.bounds.offset++] = ymax + labelMargin1
 
       out.cells.data[out.cells.offset++] = 0
       out.cells.data[out.cells.offset++] = 1
@@ -79,18 +59,26 @@ module.exports = function (params) {
       out.cells.data[out.cells.offset++] = 0
       out.cells.data[out.cells.offset++] = 2
       out.cells.data[out.cells.offset++] = 3
+      out.positions.data[out.positions.offset++] = xmin
+      out.positions.data[out.positions.offset++] = ymin
+      out.positions.data[out.positions.offset++] = xmax
+      out.positions.data[out.positions.offset++] = ymin
+      out.positions.data[out.positions.offset++] = xmax
+      out.positions.data[out.positions.offset++] = ymax
+      out.positions.data[out.positions.offset++] = xmin
+      out.positions.data[out.positions.offset++] = ymax
     },
     params: params
   }
 }
 
-function pset(X, p0, p1, p2, p3) {
-  X[0] = p0[0]
-  X[1] = p0[1]
-  X[2] = p1[0]
-  X[3] = p1[1]
-  X[4] = p2[0]
-  X[5] = p2[1]
-  X[6] = p3[0]
-  X[7] = p3[1]
+function pset(out, xmin, xmax, ymin, ymax) {
+  out[0] = xmin
+  out[1] = ymin
+  out[2] = xmax
+  out[3] = ymin
+  out[4] = xmax
+  out[5] = ymax
+  out[6] = xmin
+  out[7] = ymax
 }
